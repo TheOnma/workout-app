809d8ce92da4609345a0679563b1f8f23ca84b69
Precious Onma Adekwu <preciousadekwu1@gmail.com>
2026-01-12 12:48:55 +0100

    Added get_workout, get_workouts, create_workout functions to workout.py

--- fastapibackend/api/routers/workouts.py
+++ fastapibackend/api/routers/workouts.py
@@ -17,3 +17,19 @@ class WorkoutBase(BaseModel):
 class WorkoutCreate(WorkoutBase):
   pass
 
+@router.get('/')
+def get_workout(db: db_dependency, user: user_dependency, workout_id: int):
+  return db.query(Workout).filter(Workout.id == workout_id).first()
+
+@router.get('/workouts')
+def get_workouts(db: db_dependency, user:user_dependency):
+  return db.query(Workout).all()
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create_workout(db: db_dependency, user: user_dependency, workout: WorkoutCreate):
+  db_workout = Workout(**workout.model_dump(), user_id=user.get('id'))
+  db.add(db_workout)
+  db.commit()
+  db.refresh(db_workout)
+  return db_workout
+
